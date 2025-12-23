769673dcc06bad5136594ba3657118b7a50d04e1
Naveenkumar V <naveenkumarvaratharajan@gmail.com>
2025-12-23 08:38:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function menuButton(){
+    let menu=document.querySelector(".reslinks");
+    let icon=document.querySelector(".resicon");
+    menu.classList.toggle("open")
+    icon.classList.toggle("open")
+}
